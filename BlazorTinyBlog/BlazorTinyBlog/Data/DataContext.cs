bdd968f8d0a1afb038b47ae7f440e8a5685e8727
Dean McCoy <dean92@gmail.com>
2024-03-27 12:39:17 -0500

    Add project files.

---
+++ BlazorTinyBlog/BlazorTinyBlog/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorTinyBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTinyBlog.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<BlogPost> BlogPosts { get; set;}    
+        
+    }
+}
